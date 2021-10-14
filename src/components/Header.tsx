168a57ce95b03c04d1f8e82b74c855e64b3b3938
egory4fash <egormaksul@gmail.com>
2021-10-14 09:49:32 +0300

    socnet 14 lesson finish without multi-classes example

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import "./Header.css";
+import classes from "./Header.module.css";
 
 function Header () {
     return (
-        <header className="header">
+        <header className={classes.header}>
             <img
                 src="https://image.freepik.com/free-psd/creative-studio-business-logo-psd-template-in-steel-texture_53876-123233.jpg"/>
         </header>
