dd10d26075e9da5b84693fa460ae4c6fd58063dd
egory4fash <egormaksul@gmail.com>
2021-10-14 09:04:50 +0300

    socnet 14 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+function Header () {
+    return (
+        <header className="header">
+            <img
+                src="https://image.freepik.com/free-psd/creative-studio-business-logo-psd-template-in-steel-texture_53876-123233.jpg"/>
+        </header>
+    )
+}
+
+export default  Header;
+
