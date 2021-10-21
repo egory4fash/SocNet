1bb4d3a75e75d3cd230d916b5ce4414b76ffd65c
egory4fash <egormaksul@gmail.com>
2021-10-21 21:40:48 +0300

    socnet 19 untyped unfinished :(

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,23 @@ import Header from './components/Header/Header';
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            {/* <Profile />*/}
             <div className='app-wrapper-content'>
-                <Dialogs/>
-
+                <Route path='/Dialogs' component={Dialogs}/>
+                <Route path='/Profile' component={Profile}/>
             </div>
-
         </div>
-
-
+        </BrowserRouter>
     )
-        ;
-}
+};
 
 
-export default App;
+    export default App;
