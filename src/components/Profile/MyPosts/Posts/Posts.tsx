8a0d55ec7dab4c2733dcbe000f1ef70b124b0cbb
egory4fash <egormaksul@gmail.com>
2021-10-21 12:52:05 +0300

    socnet 18 untyped

--- src/components/Profile/MyPosts/Posts/Posts.tsx
+++ src/components/Profile/MyPosts/Posts/Posts.tsx
@@ -7,7 +7,7 @@ function Posts(props: any) {
         <div className={classes.item}>
             <img src='https://www.nj.com/resizer/iqV2J-QFgh0227ybHBor4exTVBk=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg' />
             <div>{props.message}</div>
-            <div><span>'likes: {props.likes}</span></div>
+            <div><span>Likes: {props.likes}</span></div>
 
         </div>
     )
