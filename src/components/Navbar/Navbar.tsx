038424ce06b2160612e7dea3d8d46918a74db0fb
egory4fash <egormaksul@gmail.com>
2021-10-22 20:10:54 +0300

    socnet 20 untyped unfinished :(

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
         <nav className={classes.nav}>
-            <div className ={classes.item}><a href = '/profile'>Profile</a></div>
-            <div className ={classes.item}><a href = '/dialogs'>Messages</a></div>
+            <div className ={classes.item}><NavLink to = '/profile'>Profile</NavLink></div>
+            <div className ={classes.item}><NavLink to = '/dialogs'>Messages</NavLink></div>
             <div className ={classes.item}><a>News</a></div>
             <div className ={classes.item}><a>Music</a></div>
             <div className ={classes.item}><a>Settings</a></div>
