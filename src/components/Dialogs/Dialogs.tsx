617bf09f01e9cb9033a41a9056f1f79b302f42b6
egory4fash <egormaksul@gmail.com>
2021-10-23 14:49:24 +0300

    socnet 21 untyped

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import classes from "./Dialogs.module.css";
 
 const Dialogs = (props: any) => {
-return (
-    <div>Dialogs</div>
-)
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Smth1
+                </div>
+                <div className={classes.dialog+ ' '+classes.active}>
+                    Smth2
+                </div>
+                <div className={classes.dialog}>
+                    Smth3
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className = {classes.message}>message1</div>
+                <div className = {classes.message}>message2</div>
+                <div className = {classes.message}>message3</div>
+
+            </div>
+        </div>
+    )
 }
 
 
