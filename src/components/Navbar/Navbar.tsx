4ea6b2fb80ae3ee24df50329f8db57512e5dfc31
egory4fash <egormaksul@gmail.com>
2021-10-23 14:09:30 +0300

    socnet 20 untyped

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,8 +5,12 @@ import {NavLink} from "react-router-dom";
 function Navbar() {
     return (
         <nav className={classes.nav}>
-            <div className ={classes.item}><NavLink to = '/profile'>Profile</NavLink></div>
-            <div className ={classes.item}><NavLink to = '/dialogs'>Messages</NavLink></div>
+            <div className ={classes.item}>
+                <NavLink to = '/profile' activeClassName = {classes.active}>Profile</NavLink>
+            </div>
+            <div className ={classes.item}>
+                <NavLink to = '/dialogs' activeClassName = {classes.active}>Messages</NavLink>
+            </div>
             <div className ={classes.item}><a>News</a></div>
             <div className ={classes.item}><a>Music</a></div>
             <div className ={classes.item}><a>Settings</a></div>
