5d4de9dc9b5c742489b68cfe670570514bd2045e
egory4fash <egormaksul@gmail.com>
2021-10-25 13:57:33 +0300

    socnet 23 12:33 untyped

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,31 +2,40 @@ import React from 'react'
 import classes from "./Dialogs.module.css";
 import {NavLink} from "react-router-dom";
 
+
+
+
+const DialogItem = (props: any) => {
+
+    let path = '/dialogs/'+ props.id;
+
+    return (<div className={classes.dialog}>
+        <NavLink to={path}>
+            {props.name}
+        </NavLink>
+    </div>)
+}
+
+const Message =(props: any) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
+}
+
 type DialogsPropsType = { message: string }
 const Dialogs = (props: DialogsPropsType) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/1'>
-                        Smth1
-                    </NavLink>
-                </div>
-                <div className={classes.dialog + ' ' + classes.active}>
-                    <NavLink to='/dialogs/2'>
-                        Smth2
-                    </NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/3'>
-                        Smth3
-                    </NavLink>
-                </div>
+                <DialogItem name = 'Smth1' id = '1'/>
+                <DialogItem name = 'Smth2' id = '2'/>
+                <DialogItem name = 'Smth3' id = '3'/>
+
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>message1</div>
-                <div className={classes.message}>message2</div>
-                <div className={classes.message}>message3</div>
+               <Message message='Msg1' />
+               <Message message='Msg2' />
+               <Message message='Msg3' />
 
             </div>
         </div>
