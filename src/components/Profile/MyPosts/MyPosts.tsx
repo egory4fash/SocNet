2006ff217ad31acb1feb412d0052fea4467d27b5
egory4fash <egormaksul@gmail.com>
2021-11-16 16:41:34 +0300

    24 done

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,7 +3,15 @@ import Posts from "./Posts/Posts";
 import classes from "./MyPosts.module.css";
 
 
+
 function MyPosts() {
+
+    const postsData = [
+        {id: 1, message: "1st post",likesCount :12},
+        {id: 2, message: "2nd post bro",likesCount :23},
+        {id: 3, message: "need 3rd?",likesCount :45}
+    ]
+
     return (
 
         <div className={classes.postsBlock}>
@@ -13,8 +21,10 @@ function MyPosts() {
                 <button>Add post</button>
             </div>
             <div className={classes.posts}>
-                <Posts message="Hi,how are you?" likes='15'/>
-                <Posts message="It's my first post" likes='20'/>
+                <Posts message={postsData[0].message} likes ={postsData[0].likesCount}/>
+                <Posts message={postsData[1].message} likes ={postsData[1].likesCount}/>
+                <Posts message={postsData[2].message} likes ={postsData[2].likesCount}/>
+
             </div>
         </div>
     )
